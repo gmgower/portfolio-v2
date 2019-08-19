5a17e64d318d77de45acad0b2ec2e4cc9be183f4
Mada Gower <gmgower@gmail.com>
2019-08-19 17:27:13 -0500

    Add button to top scroll.

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // 44: 15
 // const menuBar = document.querySelector('.menu-toggler')
-// // const menuBar1 = document.querySelector('.menu-toggler')
+// const navLinks = document.querySelector('.nav-list')
+// const links = document.querySelector('.nav-list li')
 
 // menuBar.addEventListener('click', function(event) {
 //     console.log(event)
-//     this.classList.toggle('open')
+//     navLinks.classList.toggle('open');
+//     links.forEach(link => {
+//       link.classList.toggle('open')
+//     })
 //     // menuBar1.toggle('open')
 // })
 
@@ -18,4 +22,23 @@ $(document).ready(function() {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
+
+$('.top-nav .nav-link').on('click', function () {
+  $('.menu-toggler').removeClass('open');
+  $('.top-nav').removeClass('open');
 });
+
+$('nav a[href*="#"]').on('click', function() {
+  $('html, body').animate(keyframes: {
+    scrollTop: $($(this).attr('href')).offset().top -100
+  }, 2000)
+});
+
+$('#up').on('click', function() {
+  $('html, body').animate(keyframes: {
+    scrollTop: 0
+  }, 2000)
+});
+
+});
+
