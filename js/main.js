109bd054220f62566707dbe2979304203513f88f
@gmgower <gmgower@gmail.com>
2020-11-05 13:25:10 -0600

    link naviagtions.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,81 @@
+/* navigation menu */
+(() => {
+  const hamburgerBtn = document.querySelector(".hamburger-btn"),
+    navMenu = document.querySelector(".nav-menu"),
+    closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+  hamburgerBtn.addEventListener("click", showNavMenu);
+  closeNavBtn.addEventListener("click", hideNavMenu);
+
+  function showNavMenu() {
+    navMenu.classList.add("open");
+    bodyScrollingToggle();
+  }
+
+  function hideNavMenu() {
+    navMenu.classList.remove("open");
+    fadeOutEffect();
+    bodyScrollingToggle();
+  }
+  function fadeOutEffect() {
+    document.querySelector(".fade-out-effect").classList.add("active");
+    setTimeout(() => {
+      document.querySelector(".fade-out-effect").classList.remove("active");
+    }, 300);
+  }
+  // attach and event handler ot document
+  document.addEventListener("click", (event) => {
+    // console.log(event.target)
+    if (event.target.classList.contains("link-item")) {
+      // console.log("event.target contains 'link-item' class");
+      // console.log(event.target.hash);
+      /* make sure event.target.hash has a value before overriding default behavior */
+      if (event.target.hash !== "") {
+        // prevent default anchor click behavior
+        event.preventDefault();
+        const hash = event.target.hash;
+        console.log(hash);
+        // deactivate existing active 'section;
+        document.querySelector(".section.active").classList.add("hide");
+        document.querySelector(".section.active").classList.remove("active");
+        // activate new 'section'
+        document.querySelector(hash).classList.add("active");
+        document.querySelector(hash).classList.remove("hide");
+        // deactivate existing active navigation menu 'link-item'
+        navMenu
+          .querySelector(".active")
+          .classList.add("outer-shadow", "hover-in-shadow");
+        navMenu
+          .querySelector(".active")
+          .classList.remove("active", "inner-shadow");
+        /* if clicked 'link-item is contained with in the navigation menu' */
+        if (navMenu.classList.contains("open")) {
+          // activate new navigation menu "link-item"
+          event.target.classList.add("active", "inner-shadow");
+          event.target.classList.remove("outer-shadow", "hover-in-shadow");
+          // hide navigation menu
+          hideNavMenu();
+          // console.log("clicked 'link-item is contained with in the navigation menu")
+        } else {
+          // console.log("clicked 'link-item is not contained with in the navigation menu'")
+          let navItems = navMenu.querySelectorAll(".link-item");
+          navItems.forEach((item) => {
+            if (hash === item.hash) {
+              // activate new navigation menu "link-item"
+              item.classList.add("active", "inner-shadow");
+              item.classList.remove("outer-shadow", "hover-in-shadow");
+            }
+          });
+          fadeOutEffect();
+        }
+        // add hash (#) to url
+        window.location.hash = hash;
+      }
+    }
+    // console.log("event.target not contains 'link-item' class");
+  });
+})();
+
 /*about section tabs */
 (() => {
   const aboutSection = document.querySelector(".about-section"),
@@ -112,7 +190,7 @@ function bodyScrollingToggle() {
   closeBtn.addEventListener("click", () => {
     popupToggle();
     popupSlideshow();
-    if(projectDetailsContainer.classList.contains("active")){
+    if (projectDetailsContainer.classList.contains("active")) {
       popupDetailsToggle();
     }
   });
@@ -171,7 +249,7 @@ function bodyScrollingToggle() {
 
   function popupDetails() {
     // if portfolio-item-detail does not exists
-    if(!portfolioItems[itemIndex].querySelector(".portfolio-item-details")){
+    if (!portfolioItems[itemIndex].querySelector(".portfolio-item-details")) {
       projectDetailsBtn.style.display = "none";
       return; /* end function execution*/
     }
@@ -193,7 +271,9 @@ function bodyScrollingToggle() {
     const category = portfolioItems[itemIndex].getAttribute("data-category");
     // console.log(category)
     // set the project category
-    popup.querySelector(".pp-project-category").innerHTML = category.split("-").join(" ");
+    popup.querySelector(".pp-project-category").innerHTML = category
+      .split("-")
+      .join(" ");
   }
 
   projectDetailsBtn.addEventListener("click", () => {
@@ -219,54 +299,69 @@ function bodyScrollingToggle() {
   }
 })();
 
-
 /* testimonial slider */
 
 (() => {
   // console.log("hello");
   const sliderContainer = document.querySelector(".testi-slider-container"),
-  slides = sliderContainer.querySelectorAll(".testi-item");
+    slides = sliderContainer.querySelectorAll(".testi-item");
   // console.log(slides);
   slideWidth = sliderContainer.offsetWidth;
   // console.log(slideWidth);
-  prevBtn = document.querySelector(".testi-slider-nav .prev"),
-  nextBtn = document.querySelector(".testi-slider-nav .next"),
-  activeSlide = sliderContainer.querySelector(".testi-item.active");
-  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+  (prevBtn = document.querySelector(".testi-slider-nav .prev")),
+    (nextBtn = document.querySelector(".testi-slider-nav .next")),
+    (activeSlide = sliderContainer.querySelector(".testi-item.active"));
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(
+    activeSlide
+  );
   // console.log(slideIndex)
 
   // set width of all sides
   slides.forEach((slide) => {
     // console.log(slide);
     slide.style.width = slideWidth + "px";
-  })
-  // set width of sliderContainer 
+  });
+  // set width of sliderContainer
   sliderContainer.style.width = slideWidth * slides.length + "px";
 
-  nextBtn.addEventListener("click", ()=> {
-    if(slideIndex === slides.length - 1){
+  nextBtn.addEventListener("click", () => {
+    if (slideIndex === slides.length - 1) {
       slideIndex = 0;
     } else {
       slideIndex++;
     }
     slider();
-  })
+  });
 
-  prevBtn.addEventListener("click", ()=>{
-    if(slideIndex === 0){
+  prevBtn.addEventListener("click", () => {
+    if (slideIndex === 0) {
       slideIndex = slides.length - 1;
     } else {
       slideIndex--;
-    }    
+    }
     slider();
-  })
+  });
 
-  function slider(){
+  function slider() {
     // deactivate existing active slide
-    sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+    sliderContainer
+      .querySelector(".testi-item.active")
+      .classList.remove("active");
     // activate new slide
-    slides[slideIndex].classList.add("active")
-    sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
+    slides[slideIndex].classList.add("active");
+    sliderContainer.style.marginLeft = -(slideWidth * slideIndex) + "px";
   }
   slider();
-})()
+})();
+
+/* hide all sections except active */
+(() => {
+  // console.log('test')
+  const sections = document.querySelectorAll(".section");
+  // console.log(sections)
+  sections.forEach((section) => {
+    if (!section.classList.contains("active")) {
+      section.classList.add("hide");
+    }
+  });
+})();
