98d349d9aec625d418f59e75207c607b813825dd
@gmgower <gmgower@gmail.com>
2020-11-07 16:11:30 -0600

    website responsive.

--- js/main.js
+++ js/main.js
@@ -365,3 +365,11 @@ function bodyScrollingToggle() {
     }
   });
 })();
+
+window.addEventListener("load", () => {
+  // preloader
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display="none";
+  }, 600)
+})
